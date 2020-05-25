0c79c7b99c5f3ad6df7f64c893c70032c452624f
4ReeX <56444853+4ReeX@users.noreply.github.com>
2020-05-26 01:07:49 +0300

    123

--- header.php
+++ header.php
@@ -1,46 +1,32 @@
 <div class="header">
 
-<nav class="navbar navbar-expand-lg navbar-dark">
+<a class="navbar-brand d-lg-none" href="#">
+    <div class="logo">
+    <img src="img/new-logo.svg" alt="" class="icon-logo">
+    <img src="img/only-affimob.svg" alt="" class="text-logo"></div>
+</a>
+<ul class="navbar-nav" id="navlist">
+  <li class="nav-item" id="index">
+    <a class="nav-link" href="index.php">Главная</a>
+  </li>
+  <li class="nav-item" id="blog">
+    <a class="nav-link" href="blog.php">Блог</a>
+  </li>
+</ul>
 
-<!--  Show this only on mobile to medium screens  -->
-  <a class="navbar-brand d-lg-none" href="#">
-  	<div class="logo">
-	  <img src="img/new-logo.svg" alt="" class="icon-logo">
-	  <img src="img/only-affimob.svg" alt="" class="text-logo"></div>
-  </a>
 
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <div class="logo">
+    <img src="img/new-logo.svg" alt="" class="icon-logo">
+    <img src="img/only-affimob.svg" alt="" class="text-logo"></div>
 
-<!--  Use flexbox utility classes to change how the child elements are justified  -->
-  <div class="collapse navbar-collapse justify-content-between" id="navbarToggle">
 
-    <ul class="navbar-nav" id="navlist">
-      <li class="nav-item" id="index">
-        <a class="nav-link" href="index.php">Главная</a>
-      </li>
-      <li class="nav-item" id="blog">
-        <a class="nav-link" href="blog.php">Блог</a>
-      </li>
-    </ul>
-    
-    
-<!--   Show this only lg screens and up   -->
-    <a class="navbar-brand d-none d-lg-block" href="#">
-    	<div class="logo">
-	  <img src="img/new-logo.svg" alt="" class="icon-logo">
-	  <img src="img/only-affimob.svg" alt="" class="text-logo"></div>
-	</a>
-    
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        	<a class="nav-link autorize cd-log" href="#0" onclick="focusMethod();">
-	   			<span class="icon-enter"></span>
-	   		    <span class="auth">Авторизация</span>
-	  		</a>
-      </li>
-    </ul>
-  </div>
-</nav>  
+<ul class="navbar-nav">
+  <li class="nav-item">
+      <a class="nav-link autorize cd-log" href="#0" onclick="focusMethod();">
+      <span class="icon-enter"></span>
+        <span class="auth">Авторизация</span>
+    </a>
+  </li>
+</ul>
+ 
 </div>
--- index.php
+++ index.php
@@ -1,29 +1,8 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="description" content="Партнёрская партнёрская программа Affimob! Партнёрка WapClick подписок - для арбитражников и вебмастеров"/>
-  <meta property="og:title" content="Аффимоб"/>
-  <meta property="og:description" content="Партнерская программа"/>
-  <meta property="og:image" content="ССЫЛКА НА КАРТИНКУ ИЗ ВИКИПЕДИИ С НАШИМ ЛОГО"/>
-  <meta property="og:type" content="profile"/>
-  <meta property="og:url" content= "ССЫЛКА НА СТАТЬЮ В ВИКИПЕДИЮ про нас" />
-  <title>Affimob - wap-click партнерская программа</title>
-  <!--<meta name="viewport" content="width=device-width">-->
-  <meta content='width=device-width, initial-scale=1.1, maximum-scale=1.1, shrink-to-fit=no, user-scalable=yes' name='viewport' />
-  <meta name="apple-mobile-web-app-capable" content="yes" />
-  <!-- Link Swiper's CSS -->
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap&subset=cyrillic,cyrillic-ext,latin-ext" rel="stylesheet">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/reset.css">
-  <link rel="stylesheet" href="css/swiper.min.css">
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="css/popup.css">
-  <link rel="stylesheet" href="css/media.css">
-  <script type="text/javascript" src="js/modernizr.js"></script>
-  <script type="text/javascript" src="js/jquery-3.5.1.min.js"></script>
-  <style>
+<?php include 'head.php'; ?>
+<style>
  input, input:before, input:after {
       -webkit-user-select: initial;
       -khtml-user-select: initial;
@@ -41,7 +20,6 @@
       .imgBox {max-width: calc((100% / 3) - 50px);}
      }
   </style>
-</head>
 <body> 
   <?php include 'header.php'; ?>
   <div class="swiper-container">
