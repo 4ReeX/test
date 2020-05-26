97d0065910e21011cb76c7f5754e4b6c2ac39c78
4ReeX <56444853+4ReeX@users.noreply.github.com>
2020-05-26 09:56:52 +0300

    2

--- news-open.php
+++ news-open.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <?php include 'head.php'; ?>
@@ -17,69 +16,6 @@
   </div>
 
 <?php include 'modal.php'; ?>
-
-<!-- JQUERY -->
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script type="text/javascript">
-  focusMethod = function getFocus() {           
-  document.getElementById("#signin-email").focus();
-}
-  focusMethodRega = function getFocus() {           
-  document.getElementById("#signup-username").focus();
-}
-</script>
-
-<!-- Initialize Swiper -->
-
-<script type="text/javascript" src="js/main.js"></script>
-<script>
-  // ---------Responsive-navbar-active-animation-----------
-function test(){
-  var tabsNewAnim = $('#navbarSupportedContent');
-  var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
-  var activeItemNewAnim = tabsNewAnim.find('.active');
-  var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
-  var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
-  var itemPosNewAnimTop = activeItemNewAnim.position();
-  var itemPosNewAnimLeft = activeItemNewAnim.position();
-  $(".hori-selector").css({
-    "top":itemPosNewAnimTop.top + "px", 
-    "left":itemPosNewAnimLeft.left + "px",
-    "height": activeWidthNewAnimHeight + "px",
-    "width": activeWidthNewAnimWidth + "px"
-  });
-  $("#navbarSupportedContent").on("click","li",function(e){
-    $('#navbarSupportedContent ul li').removeClass("active");
-    $(this).addClass('active');
-    var activeWidthNewAnimHeight = $(this).innerHeight();
-    var activeWidthNewAnimWidth = $(this).innerWidth();
-    var itemPosNewAnimTop = $(this).position();
-    var itemPosNewAnimLeft = $(this).position();
-    $(".hori-selector").css({
-      "top":itemPosNewAnimTop.top + "px", 
-      "left":itemPosNewAnimLeft.left + "px",
-      "height": activeWidthNewAnimHeight + "px",
-      "width": activeWidthNewAnimWidth + "px"
-    });
-  });
-}
-$(document).ready(function(){
-  setTimeout(function(){ test(); });
-});
-$(window).on('resize', function(){
-  setTimeout(function(){ test(); }, 500);
-});
-$(".navbar-toggler").click(function(){
-  setTimeout(function(){ test(); });
-});
-</script>
-
-
-<script>
-$(document).ready(function(){
-         $("#blog a").addClass('active');
-         $('#navlist').siblings('li').removeClass('active');
-});
-</script>
+<?php include 'scripts.php'; ?>
 </body>
 </html>
---
+++ scripts.php
@@ -0,0 +1,90 @@
+<!-- JQUERY -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script type="text/javascript">
+  focusMethod = function getFocus() {           
+  document.getElementById("#signin-email").focus();
+}
+  focusMethodRega = function getFocus() {           
+  document.getElementById("#signup-username").focus();
+}
+</script>
+
+<!-- Initialize Swiper -->
+
+<script type="text/javascript" src="js/main.js"></script>
+<script>
+  // ---------Responsive-navbar-active-animation-----------
+function test(){
+  var tabsNewAnim = $('#navbarSupportedContent');
+  var selectorNewAnim = $('#navbarSupportedContent').find('li').length;
+  var activeItemNewAnim = tabsNewAnim.find('.active');
+  var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+  var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+  var itemPosNewAnimTop = activeItemNewAnim.position();
+  var itemPosNewAnimLeft = activeItemNewAnim.position();
+  $(".hori-selector").css({
+    "top":itemPosNewAnimTop.top + "px", 
+    "left":itemPosNewAnimLeft.left + "px",
+    "height": activeWidthNewAnimHeight + "px",
+    "width": activeWidthNewAnimWidth + "px"
+  });
+  $("#navbarSupportedContent").on("click","li",function(e){
+    $('#navbarSupportedContent ul li').removeClass("active");
+    $(this).addClass('active');
+    var activeWidthNewAnimHeight = $(this).innerHeight();
+    var activeWidthNewAnimWidth = $(this).innerWidth();
+    var itemPosNewAnimTop = $(this).position();
+    var itemPosNewAnimLeft = $(this).position();
+    $(".hori-selector").css({
+      "top":itemPosNewAnimTop.top + "px", 
+      "left":itemPosNewAnimLeft.left + "px",
+      "height": activeWidthNewAnimHeight + "px",
+      "width": activeWidthNewAnimWidth + "px"
+    });
+  });
+}
+$(document).ready(function(){
+  setTimeout(function(){ test(); });
+});
+$(window).on('resize', function(){
+  setTimeout(function(){ test(); }, 500);
+});
+$(".navbar-toggler").click(function(){
+  setTimeout(function(){ test(); });
+});
+</script>
+
+
+<script>
+$(document).ready(function(){
+         $("#blog a").addClass('active');
+         $('#navlist').siblings('li').removeClass('active');
+});
+</script>
+<script>
+  function link() {
+    location.href = "news-open.php";
+  };
+</script>
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"> </script>
+<script>
+$('.share-button').on('click',function(){  
+  $(this).addClass('open');
+})
+$( ".share-items" ).draggable({ 
+  axis: "x",
+  containment : ".share-items-wrapper"
+});
+$( ".share-item" ).on('click',function(){
+   $('.share-button').addClass('shared');
+   setTimeout(function(){
+    $('.share-button').addClass('thankyou');
+  }, 800);
+  setTimeout(function(){
+    $('.share-button').removeClass('open');
+    $('.share-button').removeClass('shared');
+    $('.share-button').removeClass('thankyou');
+  }, 2500);
+});
+</script>
