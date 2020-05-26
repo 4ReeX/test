ebb0455aa261fdb42ce9930f045ea94c84b08f0c
4ReeX <56444853+4ReeX@users.noreply.github.com>
2020-05-26 11:38:42 +0300

    123

--- news-open.php
+++ news-open.php
@@ -11,49 +11,9 @@
           <?php include 'one-new.php'; ?>
         </div>
         <!--Пагинация-->
-        <nav>
-        <ul class="pagination pag3">
-        </ul>
-        </nav>
-        <script>
-          let count = 20;
-
-function indexes(selector, page, range){
-  if (page > 1){
-    $(selector).append('<li class="page-item"><a class="page-link" href="./' + (page-1) + '">Предыдущая</a></li>');
-  }
-  for (let i = 1; i <= count; i++){
-    if (i == 1 && page-range > 1){
-      $(selector).append('<li class="page-item"><a class="page-link" href="./1">1</a></li>');
-        if (page-range > 2){
-          $(selector).append('<li class="page-item disabled"><a class="page-link" href="#">...</a></li>');
-      }
-    }
-    if (i >= page-range && i <= page+range){
-      if (i != page){
-        $(selector).append('<li class="page-item"><a class="page-link" href="./' + i + '">' + i + '</a></li>');
-      } else {
-        $(selector).append('<li class="page-item active"><a class="page-link" href=""./' + i + '"">' + i + '</a></li>');
-      }
-    }
-    if (i == count && page+range < count){
-      if (page+range < count-1){
-        $(selector).append('<li class="page-item disabled"><a class="page-link" href="#">...</a></li>');
-      }
-      $(selector).append('<li class="page-item"><a class="page-link" href="./' + count + '">' + count + '</a></li>'); 
-    }
-  }
-  if (page < count){
-    $(selector).append('<li class="page-item"><a class="page-link" href="./' + (page+1) + '">Следующая</a></li>');
-  }
-}
-
-indexes('.pag1', 1, 3); 
-indexes('.pag2', 15, 3); 
-indexes('.pag3', 15, 1); 
-indexes('.pag4', count, 3)
-        </script>
-
+        <a href="" class="prev float-left"><i class="ti ti-arrow-left "></i> Предыдущая</a>
+        <a href="" class="next float-right">Следующая <i class="ti ti-arrow-right"></i></a>
+       
 
 
         </div>
